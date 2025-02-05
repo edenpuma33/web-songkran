a24c7e51f63dd85e92a8418c8cfbd4ceefa5993f
edenpuma33 <edenpuma33@gmail.com>
2025-02-05 19:03:58 +0700

    update

--- src/ShowMap.jsx
+++ src/ShowMap.jsx
@@ -1,65 +1,3 @@
-// import { useState } from "react";
-
-// const ShowMap  = () => {
-// const [activeButton, setActiveButton] = useState("Button 1");
-
-// const toggleButton = (button) => {
-// setActiveButton(activeButton === button ? button : button);
-// };
-
-// return (
-// <div className="flex items-start gap-4 p-4">
-// <div className="flex flex-col gap-2 w-1/8">
-// <button
-//     onClick={() => toggleButton("Button 1")}
-//     className={`px-4 py-2 rounded-lg map ${
-//     activeButton === "Button 1" ? "bg-blue-500 text-white" : "bg-gray-300 text-black"
-//     }`}
-// >
-//     The Original ต้นทาง
-// </button>
-
-// <button
-//     onClick={() => toggleButton("Button 2")}
-//     className={`px-4 py-2 rounded-lg map ${
-//     activeButton === "Button 2" ? "bg-blue-500 text-white" : "bg-gray-300 text-black"
-//     }`}
-// >
-//     เลือกรับ Destiny
-// </button>
-
-// <button
-//     onClick={() => toggleButton("Button 3")}
-//     className={`px-4 py-2 rounded-lg map ${
-//     activeButton === "Button 3" ? "bg-blue-500 text-white" : "bg-gray-300 text-black"
-//     }`}
-// >
-//     ปรับใช้ Locallized
-// </button>
-
-// <button 
-//     onClick={() => toggleButton("Button 4")}
-//     className={`px-4 py-2 rounded-lg map ${
-//     activeButton === "Button 4" ? "bg-blue-500 text-white" : "bg-gray-300 text-black"
-//     }`}
-// >
-//     เส้นทางการส่งต่อวัฒนธรรม
-// </button>
-// </div>
-
-// <div className="flex items-center w-7/8 justify-center">
-// <p className="text-lg font-semibold">
-// {activeButton ? `Detail: ${activeButton} `
-// : "No button selected"}
-// </p>
-// </div>
-
-// </div>
-// );
-// };
-
-// export default ShowMap
-
 import { useState } from "react";
 import photo1 from "./images/ส่ง.jpg"
 import photo2 from "./images/รับ.jpg"
@@ -71,27 +9,48 @@ const [selected, setSelected] = useState("option1");
 
 const content = {
 option1: {
-    text: "This is Image 1",
-    description: "songkran1",
+    text: "ส่ง (origin)",
     img: photo1,
-    value1:"ปฏิทินสุริยคติ – มหาศักราช – เมษสงกรานต์อิทธิพลจากอินเดีย (ผ่านสู่วัฒนธรรมปยู/พม่า/ยะไข่/มอญ และวัฒนธรรมเขมรโบราณ กระทั่งมาสู่วัฒนธรรมสุโขทัย)" ,
-    value2:"ปฏิทินดาราคติ– จุลศักราช – เมษสงกรานต์ – วันเถลิงศกอิทธิพลจากอินเดีย (ผ่านสู่วัฒนธรรมปยู (ศรีเกษตร ตั้งต้นนับจุล) ไป /พม่า/ยะไข่/มอญ และวัฒนธรรมเขมรโบราณ กระทั่งมาสู่วัฒนธรรมสุโขทัยไปลาว) -1 อินเดีย -> เมาะมะตะ -> อยุธยา -> นครศรี -> ลาว -2-> กัมพูชา อิทธิพลจากศรีลังกา (ผ่านสู่วัฒนธรรมมอญโบราณที่เมืองเมาะตะมะ และผ่านมาสู่วัฒนธรรมสุโขทัย-ล้านนา) ส่งต่อเป็นทอดๆ"
+    value1:"value/concept",
+    title1: "อิทธิพลจากอินเดีย",
+    detailt11:"ปฏิทินสุริยคติ – มหาศักราช – เมษสงกรานต์",
+    detailt12:"ปฏิทินดาราคติ – จุลศักราช – เมษสงกรานต์ – วันเถลิงศก",
+    title2: "อิทธิพลจากศรีลังกา",
+    detailt21:"พุทธศาสนาเถรวาท",
+    title3: "อิทธิพลจากจีน ",
+    detailt31: "ปฏิทินจันทรคติ - ปีนักษัตร",
 },
 option2: {
-    text: "This is Image 2",
-    description: "songkran2",
+    text: "เลือก รับ (Destination)",
     img: photo2,
+    value1:"value/concept",
+    title1: "ปฏิทินสุริยคติ – มหาศักราช – เมษสงกรานต์",
+    detailt11:"อิทธิพลจากอินเดีย (ผ่านสู่วัฒนธรรมปยู/พม่า/ยะไข่/มอญ และวัฒนธรรมเขมรโบราณ กระทั่งมาสู่วัฒนธรรมสุโขทัย) ",
+    title2: "ปฏิทินดาราคติ– จุลศักราช – เมษสงกรานต์ – วันเถลิงศก",
+    detailt21:"อิทธิพลจากอินเดีย (ผ่านสู่วัฒนธรรมปยู (ศรีเกษตร ตั้งต้นนับจุล) ไป /พม่า/ยะไข่/มอญ และวัฒนธรรมเขมรโบราณ กระทั่งมาสู่วัฒนธรรมสุโขทัยไปลาว) -1",
+    detailt22:"อิทธิพลจากศรีลังกา (ผ่านสู่วัฒนธรรมมอญโบราณที่เมืองเมาะตะมะ และผ่านมาสู่วัฒนธรรมสุโขทัย-ล้านนา) ส่งต่อเป็นทอดๆ",
+    title3: "พุทธศาสนาเถรวาท",
+    detailt31: ""
 },
 option3: {
-    text: "This is Image 3",
-    description: "songkran3",
+    text: "ปรับ ใช้ localization",
     img: photo3,
+    value1:"value/concept",
+    title1: "อิทธิพลจากอินเดีย",
+    detailt11:"ปฏิทินสุริยคติ – มหาศักราช – เมษสงกรานต์",
+    detailt12:"ปฏิทินดาราคติ – จุลศักราช – เมษสงกรานต์ – วันเถลิงศก",
+    title2: "อิทธิพลจากศรีลังกา",
+    detailt21:"พุทธศาสนาเถรวาท",
+    title3: "อิทธิพลจากจีน ",
+    detailt31: "ปฏิทินจันทรคติ - ปีนักษัตร",
 },
 option4: {
-    text: "This is Image 4",
-    description: "songkran4",
+    text: "Shared Value",
     hello: "hello",
     img: photo4,
+    value1:"value/concept",
+    value2:"tradition/ way of practice",
+    value3: "Elements"
 },
 };
 
@@ -112,8 +71,7 @@ return (
     </div>
 
     <div className="flex flex-col gap-4 items-center w-7/8 justify-center">
-    <p className="mt-4 text-lg font-semibold">{content[selected].text}</p>
-    <p className="mt-4 text-lg font-semibold">{content[selected].description}</p>
+    <p className="mt-4 text-xl font-semibold">{content[selected].text}</p>
     <p className="mt-4 text-lg font-semibold">{content[selected].hello}</p>
     <img
         src={content[selected].img}
@@ -121,7 +79,13 @@ return (
         className="mapimg w-240 h-120 rounded-lg shadow-lg"
     />
     <p className="mt-4 text-lg font-semibold">{content[selected].value1}</p>
-    <p className="mt-4 text-lg font-semibold">{content[selected].value2}</p>
+    <p className="mt-4 text-lg font-semibold">{content[selected].title1}</p>
+    <p className="mt-4 text-lg font-semibold">{content[selected].detailt11}</p>
+    <p className="mt-4 text-lg font-semibold">{content[selected].detailt12}</p>
+    <p className="mt-4 text-lg font-semibold">{content[selected].title2}</p>
+    <p className="mt-4 text-lg font-semibold">{content[selected].detailt21}</p>
+    <p className="mt-4 text-lg font-semibold">{content[selected].title3}</p>
+    <p className="mt-4 text-lg font-semibold">{content[selected].detailt31}</p>
     </div>
 </div>
 );
