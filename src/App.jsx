6ea2d250c18a922b434d5c19d91c817cf6ce2a7f
edenpuma33 <edenpuma33@gmail.com>
2025-02-07 02:15:26 +0700

    present

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import ShowMap from "./ShowMap"
 import Header from "./Header"
-import Map from "./Map"
-import Map2 from "./Map2"
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
--- src/Header.jsx
+++ src/Header.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import chulalogo from "./images/chulalogo.png"
 
 const Header = () => {
 const [menuOpen, setMenuOpen] = useState(false);
@@ -15,23 +16,24 @@ return (
 <div>
 <div className="header">
     <nav className="navbar">
-    <div className="text-2xl font-medium topbar"><h1>SONGKRANCHACHACHA</h1></div>
+    <img src={chulalogo}/>
     <div className={`nav-links ${menuOpen ? "show" : ""}`} id="navLinks">
         <i className="fa-solid fa-xmark" onClick={hideMenu}></i>
-        <ul>
-        <li><a href="/">หน้าหลัก</a></li>
-        <li><a href="/">โครงการ 1</a></li>
-        <li><a href="/">โครงการ 2</a></li>
-        <li><a href="/">โครงการ 3</a></li>
-        <li><a href="/">โครงการ 4</a></li>
-        <li><a href="/">โครงการ 5</a></li>
+        <ul className="flex gap-4">
+        <li className="rounded-3xl btn-start"><a href="/">โครงการ 1</a></li>
+        <li className="rounded-3xl btn-start"><a href="/">โครงการ 2</a></li>
+        <li className="rounded-3xl btn-start"><a href="/">โครงการ 3</a></li>
+        <li className="rounded-3xl btn-start"><a href="/">โครงการ 4</a></li>
+        <li className="rounded-3xl btn-start"><a href="/">โครงการ 5</a></li>
+        <li className="rounded-3xl btn-start "><a href="/">โครงการ 6</a></li>
+        <li className="rounded-3xl btn-start"><a href="/">เกี่ยวกับเรา</a></li>
         </ul>
     </div>
     <i className="fa-solid fa-bars" onClick={showMenu}></i>
     </nav>
 
     <div className="text-box">
-    <div className="text-6xl font-medium"><h1>SONGKRAN<br />CHACHACHA</h1></div>
+    <div className="text-6xl font-medium text-emerald-500"><h1>SONGKRAN<br />GLOBAL FESTIVAL</h1></div>
     <h4>
         แหล่งรวมความรู้เกี่ยวกับสงกรานต์วิถีใหม่ <br />
         จากโครงการของสถาบันเอเชียศึกษา จุฬาลงกรณ์มหาวิทยาลัย
